c34534dab331c51e6cf66161c01eeb776bb61f56
Jakub Arnold <darthdeus@gmail.com>
2014-05-17 18:15:26 +0200

    Initial commit

---
+++ lib/ghc/modi/wrapper.rb
@@ -0,0 +1,9 @@
+require "ghc/modi/wrapper/version"
+
+module Ghc
+  module Modi
+    module Wrapper
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/ghc/modi/wrapper/version.rb
@@ -0,0 +1,7 @@
+module Ghc
+  module Modi
+    module Wrapper
+      VERSION = "0.0.1"
+    end
+  end
+end
